13dc6f086e6662d88fc2e7f094b935af9deb2ac7
Arnav Gupta <championswimmer@gmail.com>
2022-10-09 17:41:46 +0530

    data types
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ 01-js-fundamentals/data-types.js
@@ -0,0 +1,19 @@
+let a = 10
+let b = "asdasasdasd"
+let c = "A"     // no separate char type
+let d = 10.1    // no separate float type
+let e = true
+let f = null
+let g = undefined
+
+console.log(typeof a) 
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)    // null is of 'object' type
+console.log(typeof g)
+
+// 'null', 'undefined' and 'not defined' are 3 different things in JS 
+
+

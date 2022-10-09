e58cf85d322048a1d61218d191365b5afde8677b
Arnav Gupta <championswimmer@gmail.com>
2022-10-09 18:21:02 +0530

    improve performance of for loop innerHTML
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- 02-proj1-fizzbuzz/script.js
+++ 02-proj1-fizzbuzz/script.js
@@ -1,12 +1,20 @@
 const btnPrint = document.getElementById("btnPrint");
 const inpNum = document.getElementById("inpNum");
+const listNum = document.getElementById("listNum");
 
 // DOM = document object model
 
 
 btnPrint.onclick = function() {
+  const start = Date.now()
   const num = parseInt(inpNum.value);
+  
+  let data = ''
   for (let i = 1; i <= num; i++) {
-    console.log(i)
+    data += `<li>${i}</li>`;
   }
+  listNum.innerHTML = data;
+
+  const end = Date.now()
+  console.log("time taken = " + (end - start))
 }

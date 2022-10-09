d463b333d1c4fde5a9e38d8ff0a0ef538b75d19f
Arnav Gupta <championswimmer@gmail.com>
2022-10-09 19:19:19 +0530

    FizzBuzz appending logic
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- 02-proj1-fizzbuzz/script.js
+++ 02-proj1-fizzbuzz/script.js
@@ -11,10 +11,37 @@ btnPrint.onclick = function() {
   
   for (let i = 1; i <= num; i++) {
     const li = document.createElement('li')
-    li.innerText = i
+
+    let text = ''
+    if (i % 3 === 0) text += 'fizz'
+    if (i % 5 === 0) text += 'buzz'
+    if (text === '') text = i
+    
+    li.innerText = text
+    
+    
     listNum.appendChild(li)
   }
 
   const end = Date.now()
   console.log("time taken = " + (end - start))
 }
+
+
+/**
+ * 1
+ * 2
+ * fizz
+ * 4
+ * buzz
+ * fizz
+ * 7
+ * 8
+ * fizz
+ * buzz
+ * 11
+ * fizz
+ * 13
+ * 14
+ * fizzbuzz
+ */
